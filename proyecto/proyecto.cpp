24f8522e41eaf793413585a71b6482aaf7089fc9
arturo <arturo.veliz@ucb.edu.bo>
2024-02-07 23:37:38 -0400

    mensaje_De_Bienvenida

--- proyecto/proyecto.cpp
+++ proyecto/proyecto.cpp
@@ -5,5 +5,10 @@
 int main()
 {
 	Aplicacion aplicacion;
+	cout << "Hola amigo bienvenido al sistema de supermercado" << endl;
+	cout << "-                                               -" << endl;
+	cout << "-                                               -" << endl;
+	cout << "-                                               -" << endl;
 	aplicacion.start();
+
 }
